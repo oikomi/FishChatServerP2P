e801be03eb50b64b9de412f8b59d708be6593d31
oikomi <miaohonghit@gmail.com>
2015-07-25 10:38:38 -0400

    init

--- stun/const.go
+++ stun/const.go
@@ -15,6 +15,48 @@
 
 package stun
 
+var StunServersList []string
+var dictAttrToVal map[string]string
+var dictMsgTypeToVal map[string]string
+
+var dictValToMsgType map[string]string
+
+var dictValToAttr map[string]string
+
+func init() {
+	StunServersList = []string{"stun.ekiga.net", "stunserver.org", "stun.ideasip.com", "stun.softjoys.com", "stun.voipbuster.com"}
+	
+	//dictAttrToVal := make(map[string]string)
+	
+	dictAttrToVal = map[string]string{
+		"MappedAddress": MappedAddress,
+        "ResponseAddress": ResponseAddress,
+        "ChangeRequest": ChangeRequest,
+        "SourceAddress": SourceAddress,
+        "ChangedAddress": ChangedAddress,
+        "Username": Username,
+        "Password": Password,
+        "MessageIntegrity": MessageIntegrity,
+        "ErrorCode": ErrorCode,
+        "UnknownAttribute": UnknownAttribute,
+        "ReflectedFrom": ReflectedFrom,
+        "XorOnly": XorOnly,
+        "XorMappedAddress": XorMappedAddress,
+        "ServerName": ServerName,
+        "SecondaryAddress": SecondaryAddress,
+	}
+	
+	
+	dictMsgTypeToVal = map[string]string{
+	    "BindRequestMsg": BindRequestMsg,
+	    "BindResponseMsg": BindResponseMsg,
+	    "BindErrorResponseMsg": BindErrorResponseMsg,
+	    "SharedSecretRequestMsg": SharedSecretRequestMsg,
+	    "SharedSecretResponseMsg": SharedSecretResponseMsg,
+	    "SharedSecretErrorResponseMsg": SharedSecretErrorResponseMsg,	
+	}
+}
+
 const (
 	Blocked = "Blocked"
 	OpenInternet = "Open Internet"
@@ -24,4 +66,32 @@ const (
 	RestrictPortNAT = "Restrict Port NAT"
 	SymmetricNAT = "Symmetric NAT"
 	ChangedAddressError = "Meet an error, when do Test1 on Changed IP and Port"
+)
+
+
+const (
+	//stun attributes
+	MappedAddress = "0001"
+	ResponseAddress = "0002"
+	ChangeRequest = "0003"
+	SourceAddress = "0004"
+	ChangedAddress = "0005"
+	Username = "0006"
+	Password = "0007"
+	MessageIntegrity = "0008"
+	ErrorCode = "0009"
+	UnknownAttribute = "000A"
+	ReflectedFrom = "000B"
+	XorOnly = "0021"
+	XorMappedAddress = "8020"
+	ServerName = "8022"
+	SecondaryAddress = "8050"  // Non standard extention
+	
+	//types for a stun message
+	BindRequestMsg = "0001"
+	BindResponseMsg = "0101"
+	BindErrorResponseMsg = "0111"
+	SharedSecretRequestMsg = "0002"
+	SharedSecretResponseMsg = "0102"
+	SharedSecretErrorResponseMsg = "0112"
 )
